894b130052b6a37ce5ff570a64f7d965992df6cb
haram-jang <haram.jang@webtoonscorp.com>
2023-06-10 14:39:56 +0900

    gold-star react component 분리

---
+++ helloReact/gold-star/src/components/Genshin/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+    return (
+        <footer class="footer">
+            <div class="box"></div>
+        </footer>
+    );
+}
+
+export default Footer;

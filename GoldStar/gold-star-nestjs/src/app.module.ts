d7a0111a72e7d4a0eb304b1446d87204c11d0cb7
haram-jang <haram.jang@webtoonscorp.com>
2023-06-18 14:57:39 +0900

    api 호출 test

--- GoldStar/gold-star-nestjs/src/app.module.ts
+++ GoldStar/gold-star-nestjs/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GenshineModule } from './Genshine/genshine.module';
 
 @Module({
-  imports: [],
+  imports: [GenshineModule],
   controllers: [AppController],
   providers: [AppService],
 })
